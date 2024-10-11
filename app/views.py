8972c6a66a710bedbcdb26582fa7556b13b6bf6a
Marcos Beletato <90642836+MarcosBeletato@users.noreply.github.com>
2024-10-10 22:04:02 -0300

    adding more complexity to the system
    add records functionality implemented

--- app/views.py
+++ app/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import gspread
+import datetime
 
 gc = gspread.service_account(filename='service_account.json')
 sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1UNkoZviCvDlZnoCtyKgTW3OZWtI2BTbqtHcyeuQhGdc/edit?gid=0#gid=0')
-wks = sh.worksheet('pedidos')
 
 # Create your views here.
 def home(request):
     wks = sh.worksheet('pedidos')
-    return HttpResponse(wks.acell('B2').value)
+    data = {}
+    data['values'] = wks.get_all_records()
+    return render(request, 'index.html', data)
+
+def create(request):
+    return render(request, 'create.html')
+
+def store(request):
+    sh.values_append(
+            'pedidos!A2',
+            params={
+                'valueInputOption':'RAW'
+            },
+            body={
+                'values':[[request.POST['name'], request.POST['address'], request.POST['order'], request.POST['ordertime']]]
+            }
+    )
+
+    return redirect('/')
